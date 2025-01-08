6f83693106a7148accff1efd2925f3c3b976c026
Jappie Klooster <jappieklooster@hotmail.com>
2025-01-08 22:28:55 +0100

    okay this works despite being ugly as fuck

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,72 @@
 
+use std::sync::Arc;
+use std::sync::RwLock;
+
 struct DoublyLinked<'a, Element> { 
-    head : Box<dyn Fn() -> DoublyLinked<'a, Element> + 'a>,
-    tail : Box<dyn Fn() -> DoublyLinked<'a, Element> + 'a>,
+    head : Box<dyn Fn() -> Arc<RwLock<DoublyLinked<'a, Element>>> + 'a>,
+    tail : Box<dyn Fn() -> Arc<RwLock<DoublyLinked<'a, Element>>> + 'a>,
     val: Element
 }
 
-fn singleton<'x, Element>(val: &'x Element) -> DoublyLinked<'x, &'x Element> {
-    let mut begin = DoublyLinked { 
+fn singleton<'x, Element>(val: &'x Element) -> Arc<RwLock<DoublyLinked<'x, &'x Element>>> {
+    let begin = Arc::new(RwLock::new(DoublyLinked { 
         head: Box::new(|| panic!("Head not set")),
         tail: Box::new(|| panic!("Tail not set")),
         val: val
-    };
+    }));
 
-    begin.head = Box::new(|| begin); 
-    begin.tail = Box::new(|| begin); 
+    let begin_clone = Arc::clone(&begin);
+
+    begin.write().unwrap().head = Box::new(move || Arc::clone(&begin_clone)); 
+
+    let begin_clone2 = Arc::clone(&begin);
+    begin.write().unwrap().tail = Box::new(move || Arc::clone(&begin_clone2)); 
 
     begin
 }
 
+
+fn add_tail<'x, Element>(me: Arc<RwLock<DoublyLinked<'x, &'x Element>>>, val: &'x Element) -> Arc<RwLock<DoublyLinked<'x, &'x Element>>> {
+
+    let begin = singleton(val);
+
+    let begin_clone = Arc::clone(&begin);
+    me.write().unwrap().tail = Box::new(move || Arc::clone(&begin_clone)); 
+
+    let me_clone = Arc::clone(&me);
+    begin.write().unwrap().head = Box::new(move || Arc::clone(&me_clone));
+
+    me
+}
+
+fn add_head<'x, Element>(me: Arc<RwLock<DoublyLinked<'x, &'x Element>>>, val: &'x Element) -> Arc<RwLock<DoublyLinked<'x, &'x Element>>> {
+
+    let begin = singleton(val);
+
+    let begin_clone = Arc::clone(&begin);
+    me.write().unwrap().head = Box::new(move || Arc::clone(&begin_clone)); 
+
+    let me_clone = Arc::clone(&me);
+    begin.write().unwrap().tail = Box::new(move || Arc::clone(&me_clone));
+
+    me
+}
+
 fn main() {
-    println!("Hello, world!");
+    let first = singleton(&1);
+
+    let val = ((first.read().unwrap().tail)().read().unwrap().tail)().read().unwrap().val;
+
+    println!("Hello, world! {0}", val);
+
+    let second = add_tail(first, &3);
+
+    let val3 = (second.read().unwrap().val);
+    println!("Hello, world! {0}", val3);
+
+    let val2 = (second.read().unwrap().tail)().read().unwrap().val;
+
+    println!("Hello, world! {0}", val2);
+
+
 }
